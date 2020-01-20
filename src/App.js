142d1ebbfd97c3606b2c83a2ab62c18d54e53743
Nicolas Mariscotti <nicolas.mariscotti93@gmail.com>
2020-01-20 19:03:30 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import "bootstrap/dist/css/bootstrap.min.css";
+import logo from './logo.png';
+import cookie from './cookie.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <img src={logo} alt="Frozen Dough logo" className="main-logo"/>
+      <h2>La página se encuentra en mantenimiento</h2>
+      <br />
+      <h3>¡Te pedimos disculpas por las molestias!</h3>
+      
+      <img src={cookie} alt="Rotating cookie" className="App-logo"/>
     </div>
   );
 }
